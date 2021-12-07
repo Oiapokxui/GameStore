46baae8aa74e1fe264f7efd414f7b825bcd731f7
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2021-12-06 22:21:43 -0300

    Init project

---
+++ src/test/java/usp/each/bd1/gamestore/GameStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package usp.each.bd1.gamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
