72d3e2a3f5184ccc0ecd67fbbee31b24f8c88a29
oiapokxui <enrique.emanuel@usp.br>
2021-12-17 19:05:14 -0300

    Add sql schema, data and a script to start postgres docker

---
+++ sql/start_postgres.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/docker/volumes/postgres
+rm -rf $HOME/docker/volumes/postgres/data
+
+docker run --rm --name pg-gamestore -e POSTGRES_PASSWORD=postgres -e POSTGRES_DB=dev -d -p 5432:5432  postgres
+sleep 3
+
+export PGPASSWORD=postgres
+psql -U postgres -d dev -h localhost -f schema.sql
+psql -U postgres -d dev -h localhost -f data.sql
