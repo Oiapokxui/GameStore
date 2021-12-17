6a12f04d06a798c3c27e4b66ffe5a2cb52d6b122
oiapokxui <enrique.emanuel@usp.br>
2021-12-16 22:26:20 -0300

    Clean up imports on application class

--- src/main/java/usp/each/bd1/gamestore/GameStoreApplication.java
+++ src/main/java/usp/each/bd1/gamestore/GameStoreApplication.java
@@ -1,14 +1,12 @@
 package usp.each.bd1.gamestore;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import usp.each.bd1.gamestore.data.repository.EmployeeRepository;
 
 @SpringBootApplication
 public class GameStoreApplication {
-    
+
     public static void main(String[] args) {
         SpringApplication.run(GameStoreApplication.class, args);
     }
