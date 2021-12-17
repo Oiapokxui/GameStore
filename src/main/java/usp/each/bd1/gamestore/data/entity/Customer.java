a06f5da473978fddef87c9c6818257206aaea99a
oiapokxui <enrique.emanuel@usp.br>
2021-12-16 23:42:48 -0300

    Fix column "id_cliente" still present in Customer

--- src/main/java/usp/each/bd1/gamestore/data/entity/Customer.java
+++ src/main/java/usp/each/bd1/gamestore/data/entity/Customer.java
@@ -2,6 +2,8 @@ package usp.each.bd1.gamestore.data.entity;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name="CLIENTE")
@@ -10,10 +12,6 @@ public class Customer implements Serializable {
     @Column(name="CPF")
     private String cpf;
 
-    @Id
-    @Column(name="ID_CLIENTE")
-    private long clientId;
-
     @Column(name="PONTOS_DE_FIDELIDADE")
     private int fidelityPoints;
 
@@ -21,4 +19,20 @@ public class Customer implements Serializable {
     @MapsId
     @JoinColumn(name = "CPF")
     private Person person;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Assistance> assistances;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Customer customer = (Customer) o;
+        return fidelityPoints == customer.fidelityPoints && cpf.equals(customer.cpf) && person.equals(customer.person) && Objects.equals(assistances, customer.assistances);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cpf, fidelityPoints, person, assistances);
+    }
 }
