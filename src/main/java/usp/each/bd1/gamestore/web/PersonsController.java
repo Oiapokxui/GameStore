81ddff068151bbae528db5a6aae3f025cdded8ad
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2021-12-18 12:01:51 -0300

    Add ost route for base case of deleting person

--- src/main/java/usp/each/bd1/gamestore/web/PersonsController.java
+++ src/main/java/usp/each/bd1/gamestore/web/PersonsController.java
@@ -2,6 +2,8 @@ package usp.each.bd1.gamestore.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,5 +20,11 @@ public class PersonsController {
     public Iterable<Person> getPersons() {
         var items = this.personRepository.findAll();
         return items;
+    }
+
+    @RequestMapping("/del")
+    @PostMapping
+    public void delete(@RequestBody String id) {
+        this.personRepository.deleteById(id);
     }
 }
