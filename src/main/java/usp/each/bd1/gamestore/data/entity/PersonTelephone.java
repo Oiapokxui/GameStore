3de29ac32ae114f81a0cb822d4dcb8af798f3a3f
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2021-12-17 23:43:21 -0300

    Refactor PersonTelephone

--- src/main/java/usp/each/bd1/gamestore/data/entity/PersonTelephone.java
+++ src/main/java/usp/each/bd1/gamestore/data/entity/PersonTelephone.java
@@ -1,19 +1,25 @@
 package usp.each.bd1.gamestore.data.entity;
 
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.io.Serializable;
+
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name="TELEFONE_PESSOA")
-public class PersonTelephone implements Serializable {
+public class PersonTelephone {
     @Id
     @Column(name="TELEFONE")
+    @Getter @Setter
     private String telephoneNumber;
 
-    @Id
-    @Column(name="CPF")
-    private String ownersCpf;
+    @ManyToOne
+    @JoinColumn(name = "CPF")
+    @Getter @Setter
+    private Person owner;
 }
