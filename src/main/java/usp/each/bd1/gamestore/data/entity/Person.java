75bcc0927d7dfcac8b537c905ea11be7958034d3
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2021-12-17 23:43:05 -0300

    Refactor Person

--- src/main/java/usp/each/bd1/gamestore/data/entity/Person.java
+++ src/main/java/usp/each/bd1/gamestore/data/entity/Person.java
@@ -1,39 +1,29 @@
 package usp.each.bd1.gamestore.data.entity;
 
+import static org.hibernate.annotations.CascadeType.DELETE;
+
 import javax.persistence.*;
+
+import org.hibernate.annotations.Cascade;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name="PESSOA")
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
     //TODO: Set cascade delete
     @Id
     @Column(name="CPF")
+    @Cascade(DELETE)
+    @Getter @Setter
     private String cpf;
 
     @Column(name="NOME")
+    @Getter @Setter
     private String name;
-
-    @OneToOne(mappedBy="person")
-    @PrimaryKeyJoinColumn
-    private Employee employee;
-
-    @OneToOne(mappedBy="person")
-    @PrimaryKeyJoinColumn
-    private Customer customer;
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
