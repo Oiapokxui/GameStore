5c62c4ded51b471bf158e31c98d126a73e23e957
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2021-12-18 17:09:30 -0300

    Create query for person insertion

--- src/main/java/usp/each/bd1/gamestore/data/repository/PersonRepository.java
+++ src/main/java/usp/each/bd1/gamestore/data/repository/PersonRepository.java
@@ -2,8 +2,12 @@ package usp.each.bd1.gamestore.data.repository;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import usp.each.bd1.gamestore.data.entity.Person;
@@ -12,4 +16,10 @@ import usp.each.bd1.gamestore.data.entity.Person;
 public interface PersonRepository extends CrudRepository<Person, String> {
     @Query(value="select * from pessoa", nativeQuery = true)
     List<Person> findAll();
+
+    @Transactional
+    @Modifying
+    @Query(value = "insert into pessoa values (:cpf, :nome)", nativeQuery = true)
+    void insert(@Param("cpf") String cpf, @Param("nome") String name);
+
 }
