f6893dcf73d917d6c1df024dff0273c47d09d99f
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2021-12-18 11:57:29 -0300

    Add route for deleting employee

--- src/main/java/usp/each/bd1/gamestore/web/EmployeesController.java
+++ src/main/java/usp/each/bd1/gamestore/web/EmployeesController.java
@@ -2,6 +2,8 @@ package usp.each.bd1.gamestore.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import usp.each.bd1.gamestore.data.entity.Employee;
@@ -17,5 +19,11 @@ public class EmployeesController {
     public Iterable<Employee> getEmployees() {
         var employees = this.employeeRepository.findAll();
         return employees;
+    }
+
+    @RequestMapping("/del")
+    @PostMapping
+    public void delete(@RequestBody String id) {
+        this.employeeRepository.deleteById(id);
     }
 }
