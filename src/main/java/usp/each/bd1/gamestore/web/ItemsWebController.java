413d9048f2fc275a8f46174758bba9dcd024438e
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-01-06 18:44:58 -0300

    Update storage page html and add button effect.
    The button will redirect the user to a item edit page.
    The item page is incomplete.

---
+++ src/main/java/usp/each/bd1/gamestore/web/ItemsWebController.java
@@ -0,0 +1,24 @@
+package usp.each.bd1.gamestore.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import usp.each.bd1.gamestore.data.repository.ItemRepository;
+
+@Controller
+@RequestMapping("/items")
+public class ItemsWebController {
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @GetMapping("/edit")
+    public String getItems(@RequestParam("barcode") String barcode, Model model) {
+        var item = this.itemRepository.findById(barcode);
+        model.addAttribute("item", item);
+        return (item.isPresent()) ? "items-edit" : null;
+    }
+}
--- src/main/resources/static/js/redirect.js
+++ src/main/resources/static/js/redirect.js
@@ -17,6 +17,24 @@ async function reloadToNewStorage() {
     let filterSelect = document.getElementById("filters");
     let optionIndex = filterSelect.selectedIndex;
     let storageName = filterSelect[optionIndex].label;
-    if (storageName === "Selecione um estoque") storageName = ""
-    window.location.assign("?storage=" + storageName);
+    if (storageName === "Selecione um estoque") window.location.assign("/storages")
+    else window.location.assign("?storage=" + storageName);
+}
+
+async function getFieldValueFromRowOnButtonClick(fieldId) {
+    let cells = event.target.parentNode.parentNode.children;
+    let fieldValue;
+    Array.from(cells).forEach(
+        (cell) => {
+            if (cell.id === fieldId) fieldValue = cell.innerText;
+        }
+    );
+    console.log(fieldValue);
+    return fieldValue;
+}
+
+async function editBarcode(){
+    let barcode = await getFieldValueFromRowOnButtonClick("barcode");
+
+    window.location.assign("/items/edit?barcode=" + barcode);
 }
