394a9eeb753014324e542a6ddf9a561ddebd14e8
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-01-08 04:56:12 -0300

    Searchbar now is working again!

--- src/main/resources/static/js/filter.js
+++ src/main/resources/static/js/filter.js
@@ -1,26 +1,24 @@
 (function(document) {
     'use strict';
 
-    var TableFilter = (function(myArray) {
-        var search_input;
+    let TableFilter = (function(myArray) {
+        let search_input;
 
         function _onInputSearch(e) {
             search_input = e.target;
-            var tables = document.getElementsByClassName(search_input.getAttribute('data-table'));
-            myArray.forEach.call(tables, function(table) {
-                myArray.forEach.call(table.tBodies, function(tbody) {
-                    myArray.forEach.call(tbody.rows, function(row) {
-                        var text_content = row.textContent.toLowerCase();
-                        var search_val = search_input.value.toLowerCase();
-                        row.style.display = text_content.indexOf(search_val) > -1 ? '' : 'none';
-                    });
+            let table = document.getElementById("itemsTable");
+            myArray.forEach.call(table.tBodies, function(tbody) {
+                myArray.forEach.call(tbody.rows, function(row) {
+                    var text_content = row.textContent.toLowerCase();
+                    var search_val = search_input.value.toLowerCase();
+                    row.style.display = text_content.indexOf(search_val) > -1 ? '' : 'none';
                 });
             });
         }
 
         return {
             init: function() {
-                var inputs = document.getElementsByClassName('search-input');
+                var inputs = document.getElementsByClassName('form-control');
                 myArray.forEach.call(inputs, function(input) {
                     input.oninput = _onInputSearch;
                 });
@@ -35,3 +33,28 @@
     });
 
 })(document);
+function myFunction() {
+  // Declare variables
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchBar");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("itemsTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    tds = tr[i].getElementsByTagName("td");
+    Array.from(tds).forEach( (td) => {
+            if (td) {
+                txtValue = td.textContent || td.innerText;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    )
+
+  }
+}
