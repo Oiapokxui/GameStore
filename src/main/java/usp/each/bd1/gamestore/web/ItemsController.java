b5a3fdecdec5122fb113388835f2883e27c8e0eb
Emanuel Tavares <enriqueemanuelcontato@gmail.com>
2022-01-08 19:17:43 -0300

    Remove unused dep

--- src/main/java/usp/each/bd1/gamestore/web/ItemsController.java
+++ src/main/java/usp/each/bd1/gamestore/web/ItemsController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-
 import usp.each.bd1.gamestore.data.entity.Item;
 import usp.each.bd1.gamestore.data.entity.Storage;
 import usp.each.bd1.gamestore.data.repository.ItemRepository;
